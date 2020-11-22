38c43cc32e59ed3ec2bc2e13ec9ee09ac91f4007
Gusna Ikhsan <gusna.ikhsan7@gmail.com>
2020-11-23 03:24:25 +0700

    Update app_template.blade.php

--- resources/views/layout_template/app_template.blade.php
+++ resources/views/layout_template/app_template.blade.php
@@ -20,7 +20,9 @@
 
     <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet">
     <!--CSS============================================= -->
+    {{-- <link rel="stylesheet" href="{{url('assets/template_components/css/Linearicons.css')}}"> --}}
     <link rel="stylesheet" href="https://peta-mahulu.herokuapp.com/assets/template_components/css/linearicons.css">
+    {{-- <link rel="stylesheet" href="{{url('assets/template_components/css/font-awesome.min.css')}}"> --}}
     <link rel="stylesheet" href="https://peta-mahulu.herokuapp.com/assets/template_components/css/font-awesome.min.css">
     {{-- <link rel="stylesheet" href="{{url('assets/template_components/css/bootstrap.css')}}"> --}}
     <link rel="stylesheet" href="https://peta-mahulu.herokuapp.com/assets/template_components/css/bootstrap.css">
